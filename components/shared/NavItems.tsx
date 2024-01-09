8d134c88abe16fb2903838f46ad6e94081399a1d
lynasty <lynasty@gmail.com>
2024-01-09 16:29:13 +0100

    Header Footer

--- components/shared/NavItems.tsx
+++ components/shared/NavItems.tsx
@@ -1,15 +1,18 @@
-'use client'
-import { headerLinks } from '@/constants'
-import Link from 'next/link'
-import { usePathname } from 'next/navigation'
+"use client";
+
+import { headerLinks } from "@/constants";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
 
 const NavItems = () => {
   const pathname = usePathname();
 
   return (
-    <ul className='md:flex-between flex w-full flex-col items-start gap-5 md:flex-row'>
+    <ul className="md:flex-between flex w-full flex-col items-start gap-5 md:flex-row">
       {headerLinks.map((link) => {
         const isActive = pathname === link.route;
+
         return (
           <li
             key={link.route}
@@ -17,14 +20,12 @@ const NavItems = () => {
               isActive && "text-primary-500"
             } flex-center p-medium-16 whitespace-nowrap`}
           >
-            <Link href={link.route}>
-              {link.label}
-            </Link>
+            <Link href={link.route}>{link.label}</Link>
           </li>
         );
       })}
     </ul>
-  )
-}
+  );
+};
 
-export default NavItems
+export default NavItems;
