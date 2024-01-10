a0e82284a8647336fa4fc3edee14332f9842dbfc
lynasty <lynasty@gmail.com>
2024-01-10 15:46:25 +0100

    Models

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,17 +1,36 @@
-'use server'
+"use server";
 
-import { CreateUserParams, UpdateUserParams } from "@/types"
-import { handleError } from "../utils"
-import { connectToDatabase } from "../mongodb/database"
-import User from "../mongodb/database/models/user.model"
+import { revalidatePath } from "next/cache";
 
-export const createUser = async (user: CreateUserParams) => {
+import { connectToDatabase } from "@/lib/database";
+import User from "@/lib/database/models/user.model";
+import Order from "@/lib/database/models/order.model";
+import Event from "@/lib/database/models/event.model";
+import { handleError } from "@/lib/utils";
+
+import { CreateUserParams, UpdateUserParams } from "@/types";
+
+export async function createUser(user: CreateUserParams) {
   try {
     await connectToDatabase();
+
     const newUser = await User.create(user);
     return JSON.parse(JSON.stringify(newUser));
   } catch (error) {
-    handleError(error)
+    handleError(error);
+  }
+}
+
+export async function getUserById(userId: string) {
+  try {
+    await connectToDatabase();
+
+    const user = await User.findById(userId);
+
+    if (!user) throw new Error("User not found");
+    return JSON.parse(JSON.stringify(user));
+  } catch (error) {
+    handleError(error);
   }
 }
 
